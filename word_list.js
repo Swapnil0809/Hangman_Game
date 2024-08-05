f5d238c090460cf115eab41a13166999f9649631
Swapnil0809 <swapniljadhav22811@gmail.com>
2024-08-05 15:14:12 +0530

    initial commit

---
+++ word_list.js
@@ -0,0 +1,54 @@
+const wordlist =[
+    {
+        word: "html",
+        hint: "The standard markup language for creating web pages"
+    },
+    {
+        word: "css",
+        hint: "Stylesheets used to design the look and layout of web pages"
+    },
+    {
+        word: "javascript",
+        hint: "Programming language for creating interactive web pages"
+    },
+    {
+        word: "div",
+        hint: "A generic container in HTML used to group elements"
+    },
+     {
+        word: "link",
+        hint: "HTML element to connect external resources like stylesheets"
+    },
+    {
+        word: "button",
+        hint: "Clickable element to submit forms or trigger events"
+    },
+    {
+        word: "media",
+        hint: "CSS rule used for responsive design to apply styles based on screen size"
+    },
+    {
+        word: "http",
+        hint: "A protocol for fetching resources across the web"
+    },
+    {
+        word: "github",
+        hint: "A platform for hosting and collaborating on Git repositories"
+    },
+    {
+        word: "npm",
+        hint: "A package manager for JavaScript"
+    },
+    {
+        word: "bootstrap",
+        hint: "A CSS framework for building responsive web pages"
+    },
+    {
+        word: "react",
+        hint: "A popular JavaScript library for building user interfaces"
+    },
+    {
+        word: "table",
+        hint: "HTML element for tabular data"
+    }
+];
